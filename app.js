f6efc896941615e47743b4e58d8b1c8ff8194919
WataruTomita <tomi.symphonia@gmail.com>
2021-01-19 17:46:34 +0900

    function

--- app.js
+++ app.js
@@ -1,11 +1,4 @@
-var i;
-var num = 0;
-for(i = 1; i < 11; i++){
-  num = num + i;
+function addString(strA){
+  var addStr = "Hello " + strA;
+  return addStr;
 }
-alert('1から10まで足し算した結果は' + num + 'です');
-
-alert(1 + 1);
-alert(1 - 1);
-alert(5 * 5);
-alert(6 / 2);
